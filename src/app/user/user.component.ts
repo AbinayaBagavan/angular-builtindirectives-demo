2f3e18b246fec59a043eb674553a9d5946a2d492
Abinaya Bagavan (Digital) <AB20024003@wipro.com>
2018-12-07 17:18:43 +0530

    initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl:'./user.component.html',
+})
+export class UserComponent  {
+  userDetails:any=[{ id: 1, name: 'Rajesh' },
+  { id: 2, name: 'Ritesh' },
+  { id: 3, name: 'Snephal' }];
+
+}
